20af8726cdedb053a6ec0a602a64b5c56a2ff3d0
David Byun <byundavid@naver.com>
2023-07-11 07:20:41 +0900

    redis study

--- src/main/java/com/mang/atdd/redis/RedisController.java
+++ src/main/java/com/mang/atdd/redis/RedisController.java
@@ -2,9 +2,9 @@ package com.mang.atdd.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,20 @@ public class RedisController {
     @PostMapping("/save/repository")
     public Long saveByRedisRepository(@RequestBody RedisCrudSaveRequestDto requestDto) {
         return redisCrudService.saveByRedisRepository(requestDto);
+    }
+
+    @GetMapping("/get/repository/{id}")
+    public RedisByRepositoryResponseDto getByRedisRepository(@PathVariable Long id) {
+        return redisCrudService.getByRedisRepository(id);
+    }
+
+    @PostMapping("/save/template")
+    public Boolean saveByRedisTemplate(@RequestParam String name, @RequestParam double count) {
+        return redisCrudService.saveByRedisTemplate(name, count);
+    }
+
+    @GetMapping("/get1To9/template")
+    public List<RedisByTemplateResponseDto> get1To9ByRedisTemplate() {
+        return redisCrudService.get1To9ByRedisTemplate();
     }
 }
